cc9c9dcdbe6879623aaf7a165a832d71cafe57d7
Volodymyr Komarov <vovikems@gmail.com>
2020-06-07 16:24:43 +0300

    Refactor mails.

--- app/src/Mail/TestMail.php
+++ app/src/Mail/TestMail.php
@@ -4,38 +4,16 @@ declare(strict_types = 1);
 
 namespace App\Mail;
 
-use App\Database\User;
 use App\Service\Mailer\Mail;
 
-class TestMail extends Mail
+class TestMail extends UserMail
 {
     /**
-     * @var \App\Database\User
-     */
-    public $user;
-
-    /**
-     * TestMail constructor.
-     *
-     * @param \App\Database\User $user
-     */
-    public function __construct(User $user)
-    {
-        parent::__construct();
-
-        $this->user = $user;
-    }
-
-    /**
      * {@inheritDoc}
-     *
-     * @return \App\Service\Mailer\Mail
      */
     public function build(): Mail
     {
-        return $this->subject('Test mail')
-                    ->from('test@cash-track.ml', 'Support Manager')
-                    ->to($this->user->email, $this->user->fullName())
-                    ->view('mail/test');
+        return parent::build()->subject('Test mail')
+                              ->view('mail/test');
     }
 }
