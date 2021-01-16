25b17df8c96c216ec6e9c82bac1ffdae54ce4167
Volodymyr Komarov <vovikems@gmail.com>
2021-01-16 23:41:39 +0200

    Implement commands for move data from old database schema to new

--- app/config/database.php
+++ app/config/database.php
@@ -8,6 +8,7 @@ return [
     'default'   => 'default',
     'databases' => [
         'default' => ['driver' => 'default'],
+        'old' => ['driver' => 'old'],
     ],
     'drivers'   => [
         'default' => [
@@ -15,6 +16,12 @@ return [
             'connection' => sprintf('mysql:host=%s;dbname=%s', env('DB_HOST'), env('DB_NAME')),
             'username'   => env('DB_USER'),
             'password'   => env('DB_PASSWORD'),
+        ],
+        'old' => [
+            'driver'     => Driver\MySQL\MySQLDriver::class,
+            'connection' => sprintf('mysql:host=%s;dbname=%s', env('DB_OLD_HOST'), env('DB_OLD_NAME')),
+            'username'   => env('DB_OLD_USER'),
+            'password'   => env('DB_OLD_PASSWORD'),
         ],
     ],
 ];
