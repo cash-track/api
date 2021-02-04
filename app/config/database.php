b8e4df4a806165099392a69c7357633901187475
Volodymyr Komarov <vovikems@gmail.com>
2021-02-04 21:12:07 +0200

    Remove old database connection

--- app/config/database.php
+++ app/config/database.php
@@ -8,7 +8,6 @@ return [
     'default'   => 'default',
     'databases' => [
         'default' => ['driver' => 'default'],
-//        'old' => ['driver' => 'old'],
     ],
     'drivers'   => [
         'default' => [
@@ -16,15 +15,6 @@ return [
             'connection' => sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', env('DB_HOST'), env('DB_NAME')),
             'username'   => env('DB_USER'),
             'password'   => env('DB_PASSWORD'),
-            'options' => [
-                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8mb4"',
-            ],
-        ],
-        'old' => [
-            'driver'     => Driver\MySQL\MySQLDriver::class,
-            'connection' => sprintf('mysql:host=%s;dbname=%s;charset=utf8mb4', env('DB_OLD_HOST'), env('DB_OLD_NAME')),
-            'username'   => env('DB_OLD_USER'),
-            'password'   => env('DB_OLD_PASSWORD'),
             'options' => [
                 PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES "utf8mb4"',
             ],
