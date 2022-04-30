558a1d6bf481696103693353fd88aa1a99143aed
Volodymyr Komarov <vovikems@gmail.com>
2022-04-30 17:54:24 +0300

    Fix FirebaseBootloader test in Github Actions environment

--- tests/Feature/Config/FirebaseConfigTest.php
+++ tests/Feature/Config/FirebaseConfigTest.php
@@ -5,13 +5,30 @@ declare(strict_types=1);
 namespace Tests\Feature\Config;
 
 use App\Config\FirebaseConfig;
+use Tests\Fixtures;
 use Tests\TestCase;
 
 class FirebaseConfigTest extends TestCase
 {
-    public function testGetUsersVerifyType(): void
+    public function testConfigs(): void
     {
         $config = $this->getContainer()->get(FirebaseConfig::class);
+
+        $class = new \ReflectionClass($config);
+        $class->getProperty('config')->setValue($config, [
+            'databaseUri'             => Fixtures::url(),
+            'storageBucket'           => Fixtures::string(),
+            'projectId'               => Fixtures::string(),
+            'privateKeyId'            => Fixtures::string(),
+            'privateKey'              => Fixtures::string(),
+            'clientEmail'             => Fixtures::email(),
+            'clientId'                => Fixtures::string(),
+            'authUri'                 => Fixtures::url(),
+            'tokenUri'                => Fixtures::url(),
+            'authProviderX509CertUrl' => Fixtures::url(),
+            'clientX509CertUrl'       => Fixtures::url(),
+        ]);
+
         $this->assertNotEmpty($config->getDatabaseUri());
         $this->assertNotEmpty($config->getStorageBucket());
         $this->assertNotEmpty($config->getProjectId());
