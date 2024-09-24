474e1ec3f51d394122d96f59bc80bc2c65a22e6e
vokomarov <vovikems@gmail.com>
2024-09-24 22:35:32 +0300

    Add encrypter specific tests

---
+++ tests/Feature/Service/Encrypter/AES256ECBTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Service\Encrypter;
+
+use App\Service\Encrypter\AES256ECB;
+use Spiral\Encrypter\Exception\EncrypterException;
+use Tests\Fixtures;
+use Tests\TestCase;
+
+class AES256ECBTest extends TestCase
+{
+    public function testEncryptDecrypt(): void
+    {
+        $key = Fixtures::string();
+        $message = Fixtures::string(256);
+
+        $encrypter = new AES256ECB();
+        $encrypted = $encrypter->encrypt($message, $key);
+
+        $this->assertNotEmpty($encrypted);
+        $this->assertNotEquals($message, $encrypted);
+        $this->assertEquals($message, $encrypter->decrypt($encrypted, $key));
+    }
+
+    public function testDecryptError(): void
+    {
+        $key = Fixtures::string();
+        $message = Fixtures::string(256);
+
+        $encrypter = new AES256ECB();
+        $encrypted = $encrypter->encrypt($message, $key);
+
+        $this->expectException(EncrypterException::class);
+
+        $this->assertEquals($message, $encrypter->decrypt($encrypted, $key . Fixtures::string(1)));
+    }
+}
